8f8ebf94b484dbd120b1df1579c64ccc76fc8ec2
randallard <randallard@yahoo.com>
2025-10-17 13:54:23 -0700

    fix playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './src/test/e2e',
+  testMatch: '**/*.e2e.ts',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
